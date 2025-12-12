7ce1e48f4e2e4b6f6c0192f7297488d6ca480222
rahmonbelmamadsaloev <rahmonbelmamadsaloev@MacBook-Air-rahmonbelmamadsaloev.local>
2025-12-12 15:50:04 +0500

    add swagger

--- server/urls.py
+++ server/urls.py
@@ -20,9 +20,30 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-
+schema_view = get_schema_view(
+    openapi.Info(
+        title="My SWAGGER",
+        default_version='v1',
+        description="API documentation",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path ('book/',include('book.urls')),
+
+    re_path(
+        r'^swagger/$',
+        schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui'
+    ),
+
+    # ReDoc
+    re_path(
+        r'^redoc/$',
+        schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc'
+    ),
 ]
